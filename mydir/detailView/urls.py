3608ba3daa95d909aad3c1df4a3a3e44de64db78
KawsarHamid99 <kawsarhamid7225@gmail.com>
2022-10-11 17:51:27 +0600

    updated v2

--- mydir/detailView/urls.py
+++ mydir/detailView/urls.py
@@ -3,5 +3,9 @@ from . import views
 app_name='book'
 urlpatterns=[ 
     path("",views.IndexView.as_view(),name="home"),
-    path('<slug:slug>/',views.bookDetailView.as_view(),name="details")
+    path('<slug:slug>/',views.bookDetailView.as_view(),name="details"),
+    path("bookl",views.Booklist.as_view(),name="booklist"),
+    path('isbn/<str:isbn>/',views.IsbnView.as_view(),name="isbn")
+
+
 ]
--- mydir/detailView/views.py
+++ mydir/detailView/views.py
@@ -1,11 +1,31 @@
 from itertools import count
 from multiprocessing import context
 from django.shortcuts import render,HttpResponse
-from django.views.generic import TemplateView,DetailView
+from django.views.generic import TemplateView,DetailView,ListView
 from .models import Book
 from django.db.models import F 
 from django.utils import timezone
 # Create your views here.
+
+
+class Booklist(ListView):
+    model = Book
+    template_name = 'listview2.html'
+    context_object_name = 'book'
+    paginate_by=1
+    #queryset=Book.objects.all()[:2]
+    def get_queryset(self):
+        return Book.objects.all()[:3]
+
+
+class IsbnView(ListView):
+    model=Book
+    template_name="listview2.html"
+    context_object_name='book'
+    paginate_by=2
+
+
+
 
 class IndexView(TemplateView):
     template_name='booklist.html'
@@ -15,6 +35,7 @@ class IndexView(TemplateView):
         print("--------------------------------------")
         print(context['book'])
         return context
+
 
 
 
